da16493caea098eeba8946dcb2139dacbac35d3a
teligramboturl <teligramboturl@gmail.com>
2025-10-05 18:25:06 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,7 +4,7 @@ from os import environ
 # API Configuration
 API_ID = int(os.environ.get("API_ID", "23673651"))
 API_HASH = os.environ.get("API_HASH", "f032bfa12ee46e1283f6fb23cfca5c6b")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7822989584:AAHnYAe8jQIhVQwmf3O5ro5Q-U1BknWasBc")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 CREDIT = os.environ.get("CREDIT", "♻️palsab♻️")
 # MongoDB Configuration
@@ -59,6 +59,7 @@ Please contact the admin @ItsUGBot to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
 
 
 
