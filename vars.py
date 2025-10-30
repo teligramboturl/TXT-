00128d4ef87e45d7b36b31cd018d1c5660cfd5bd
teligramboturl <teligramboturl@gmail.com>
2025-10-30 19:01:19 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,9 +4,9 @@ from os import environ
 # API Configuration
 API_ID = int(os.environ.get("API_ID", "23673651"))
 API_HASH = os.environ.get("API_HASH", "f032bfa12ee46e1283f6fb23cfca5c6b")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7822989584:AAG_plaU4BvJEF0hYsGYxpJWwhhhFTq3Ooo")
 
-CREDIT = os.environ.get("CREDIT", "♻️palsab♻️")
+CREDIT = os.environ.get("CREDIT", "♻️boss♻️")
 # MongoDB Configuration
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "GABBAR")
 DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://dubeyji0995:Gqaqduzpm98kT6ZF@cluster0.ekxizvl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # Add your own atlas db
@@ -59,6 +59,7 @@ Please contact the admin @ItsUGBot to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
 
 
 
